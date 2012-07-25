c0445d4f352e5e6a75fb959dca3dafe8b8c6e862
procrastinative.developer <procrastinative.developer@gmail.com>
2012-07-25 22:37:23 +0200

    #refactoring - use same naming convention (Jcrop not JCrop)

--- Jcrop-parent/Jcrop-examples/src/main/java/jcrop/HomePage.java
+++ Jcrop-parent/Jcrop-examples/src/main/java/jcrop/HomePage.java
@@ -7,7 +7,7 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.PackageResourceReference;
 import org.jcrop.Coordinates;
 import org.jcrop.CroppableSettings;
-import org.jcrop.JCropImage;
+import org.jcrop.JcropImage;
 import org.jcrop.PreviewSettings;
 
 public class HomePage extends WebPage {
@@ -15,37 +15,37 @@ public class HomePage extends WebPage {
 
     public HomePage(final PageParameters parameters) {
 	super(parameters);
-        add(new JCropImage("example1", new PackageResourceReference(HomePage.class, "example.jpg"), new CroppableSettings()));
+        add(new JcropImage("example1", new PackageResourceReference(HomePage.class, "example.jpg"), new CroppableSettings()));
 
         CroppableSettings settings2 = new CroppableSettings();
         settings2.setBgColor("yellow");
         settings2.setBgOpacity(0.2);
-        add(new JCropImage("example2", new PackageResourceReference(HomePage.class, "example.jpg"), settings2));
+        add(new JcropImage("example2", new PackageResourceReference(HomePage.class, "example.jpg"), settings2));
 
         CroppableSettings settings3 = new CroppableSettings();
         settings3.setBgColor("green");
         settings3.setSelect(new Coordinates(10, 10, 100, 100));
-        add(new JCropImage("example3", new PackageResourceReference(HomePage.class, "example.jpg"), settings3));
+        add(new JcropImage("example3", new PackageResourceReference(HomePage.class, "example.jpg"), settings3));
 
         CroppableSettings settings4 = new CroppableSettings();
         settings4.setAspectRatio(0.5);
-        add(new JCropImage("example4", new PackageResourceReference(HomePage.class, "example.jpg"), settings4));
+        add(new JcropImage("example4", new PackageResourceReference(HomePage.class, "example.jpg"), settings4));
 
         CroppableSettings settings5 = new CroppableSettings();
         settings5.setAspectRatio(1);
         settings5.setPreview(new PreviewSettings("preview", 100, 100));
-        add(new JCropImage("example5", new PackageResourceReference(HomePage.class, "example.jpg"), settings5));
+        add(new JcropImage("example5", new PackageResourceReference(HomePage.class, "example.jpg"), settings5));
 
         CroppableSettings settings6 = new CroppableSettings();
         settings6.setBgColor("blue");
         settings6.setBgOpacity(0.2);
         settings6.setBoxWidth(350);
-        add(new JCropImage("example6", new PackageResourceReference(HomePage.class, "example.jpg"), settings6));
+        add(new JcropImage("example6", new PackageResourceReference(HomePage.class, "example.jpg"), settings6));
 
          CroppableSettings settings7 = new CroppableSettings();
          settings7.setBgColor("pink");
          settings7.setProvideApiController(true);
-         final JCropImage jcropImage7 = new JCropImage("example7", new PackageResourceReference(HomePage.class, "example.jpg"), settings7);
+         final JcropImage jcropImage7 = new JcropImage("example7", new PackageResourceReference(HomePage.class, "example.jpg"), settings7);
 
         AjaxLink<Void> setSelectionLink = new AjaxLink<Void>("setSelection") {
             @Override
