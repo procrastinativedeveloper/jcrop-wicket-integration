2cd022460be32aa4347af091b06b049c8e75085b
procrastinative.developer <procrastinative.developer@gmail.com>
2012-07-25 22:49:40 +0200

    add animateTo(), release(), disable(), enable(), destroy() methods from jcrop api

--- Jcrop-parent/Jcrop-integration/src/main/java/org/jcrop/JcropController.java
+++ Jcrop-parent/Jcrop-integration/src/main/java/org/jcrop/JcropController.java
@@ -23,4 +23,50 @@ public class JcropController implements Serializable {
         }
         target.appendJavaScript(jcropVariableName + ".setSelect("+coordinates.toJsArray() + ");");
     }
+
+    public void animateTo(Coordinates coordinates, AjaxRequestTarget target) {
+        if ( null == coordinates) {
+            throw new IllegalArgumentException("Coordinates shoudn't be null");
+        }
+
+        if ( null == target ) {
+            throw new IllegalArgumentException("AjaxRequestTarget shoudn't be null");
+        }
+        target.appendJavaScript(jcropVariableName + ".animateTo("+coordinates.toJsArray() + ");");
+    }
+
+    /**
+     * Clear the selection
+     * @param target
+     */
+    public void release (AjaxRequestTarget target) {
+        if ( null == target ) {
+            throw new IllegalArgumentException("AjaxRequestTarget shoudn't be null");
+        }
+        target.appendJavaScript(jcropVariableName+ ".relase();");
+    }
+
+    public void disable(AjaxRequestTarget target){
+        if ( null == target ) {
+            throw new IllegalArgumentException("AjaxRequestTarget shoudn't be null");
+        }
+        target.appendJavaScript(jcropVariableName+".disable();");
+    }
+
+    public void enable(AjaxRequestTarget target){
+        if ( null == target ) {
+            throw new IllegalArgumentException("AjaxRequestTarget shoudn't be null");
+        }
+        target.appendJavaScript(jcropVariableName+".enable();");
+    }
+
+    public void destroy(AjaxRequestTarget target){
+        if ( null == target ) {
+            throw new IllegalArgumentException("AjaxRequestTarget shoudn't be null");
+        }
+        target.appendJavaScript(jcropVariableName+".destroy();");
+    }
+
+
+
 }
