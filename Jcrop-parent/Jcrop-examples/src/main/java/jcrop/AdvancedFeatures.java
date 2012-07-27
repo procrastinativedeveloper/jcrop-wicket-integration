c6bf860f8a1b330373055a37f16b53b764c5dd05
procrastinative.developer <procrastinative.developer@gmail.com>
2012-07-27 19:24:07 +0200

    use of fluent interfaces in advanced samples

--- Jcrop-parent/Jcrop-examples/src/main/java/jcrop/AdvancedFeatures.java
+++ Jcrop-parent/Jcrop-examples/src/main/java/jcrop/AdvancedFeatures.java
@@ -14,21 +14,19 @@ public class AdvancedFeatures extends WebPage {
     public AdvancedFeatures(final PageParameters parameters) {
 	super(parameters);
 
-        CroppableSettings settings4 = new CroppableSettings();
-        settings4.setAspectRatio(0.5);
+        CroppableSettings settings4 = new CroppableSettings().setAspectRatio(0.5);
         add(new JcropImage("example4", new PackageResourceReference(HomePage.class, "example.jpg"), settings4));
 
-        CroppableSettings settings5 = new CroppableSettings();
-        settings5.setAspectRatio(1.0);
-        settings5.setPreview(new PreviewSettings("preview", 100, 100));
+        CroppableSettings settings5 = new CroppableSettings()
+                    .setAspectRatio(1.0)
+                    .setPreview(new PreviewSettings("preview", 100, 100));
         add(new JcropImage("example5", new PackageResourceReference(HomePage.class, "example.jpg"), settings5));
 
-        CroppableSettings settings6 = new CroppableSettings();
-        settings6.setBgColor("blue");
-        settings6.setBgOpacity(0.2);
-        settings6.setBoxWidth(350);
+        CroppableSettings settings6 = new CroppableSettings()
+            .setBgColor("blue")
+            .setBgOpacity(0.2)
+            .setBoxWidth(350);
         add(new JcropImage("example6", new PackageResourceReference(HomePage.class, "example.jpg"), settings6));
-
 
         add(new Link<Void>("return_to_menu") {
 
